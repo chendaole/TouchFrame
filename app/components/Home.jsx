bbeda247799dfcf2c67276d319c7b33e6d55dff7
chendaole <chendaole@4399inc.com>
2016-06-27 18:36:23 +0800

    add：Silder、Group

--- app/components/Home.jsx
+++ app/components/Home.jsx
@@ -1,24 +1,55 @@
 import React from 'react';
-import {Container, View} from 'amazeui-touch';
+import {Container, Slider ,Group} from 'amazeui-touch';
 
 import './Home.css';
+
+const data = [
+    {
+        thumb: 'http://s.amazeui.org/media/i/demos/bing-1.jpg',
+        img: 'http://s.amazeui.org/media/i/demos/bing-1.jpg'
+    },
+    {
+        thumb: 'http://s.amazeui.org/media/i/demos/bing-2.jpg',
+        img: 'http://s.amazeui.org/media/i/demos/bing-2.jpg'
+    },
+    {
+        thumb: 'http://s.amazeui.org/media/i/demos/bing-3.jpg',
+        img: 'http://s.amazeui.org/media/i/demos/bing-3.jpg'
+    },
+    {
+        thumb: 'http://s.amazeui.org/media/i/demos/bing-4.jpg',
+        img: 'http://s.amazeui.org/media/i/demos/bing-4.jpg'
+    }];
 
 export default class Home extends React.Component{
     constructor(props){
         super(props);
     }
 
+    renderSlider(){
+        return(
+            <Slider>
+                {data.map((item,i) => {
+                    return(
+                        <Slider.Item
+                            key={i}
+                            thumbnail={item.thumb}>
+                            <img src={item.img}/>
+                        </Slider.Item>
+                        );
+                    })}
+            </Slider>
+        );
+
+    }
+
     render(){
         return(
                  <Container
                     transition = 'rfl'>
-                    <h1>主页</h1>
-                    <h2>测试</h2>
-                    <h2>测试</h2>
-                    <h2>测试</h2>
-                    <h2>测试</h2>
-                    <h2>测试</h2>
-                    <h2>测试</h2>
+                    <Group>
+                        {this.renderSlider()}
+                    </Group>
                 </Container>
         );
     }
