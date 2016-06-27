4a287123cfd25ca4a61d489f4e61ff1d2f977288
chendaole <1174250185@qq.com>
2016-06-27 22:08:33 +0800

    add:Card

--- app/components/Home.jsx
+++ app/components/Home.jsx
@@ -1,9 +1,35 @@
 import React from 'react';
-import {Container, Slider ,Group} from 'amazeui-touch';
+import {Container, Slider ,Group, Card} from 'amazeui-touch';
 
 import './Home.css';
 
-const data = [
+const CardData = [
+    {
+        title:'测试1',
+        content:'测试测试测试测试测试'
+    },
+    {
+        title:'测试2',
+        content:'测试测试测试测试测试'
+    },
+    {
+        title:'测试3',
+        content:'测试测试测试测试测试'
+    },{
+        title:'测试4',
+        content:'测试测试测试测试测试'
+    },
+    {
+        title:'测试5',
+        content:'测试测试测试测试测试'
+    },
+    {
+        title:'测试6',
+        content:'测试测试测试测试测试'
+    }
+]
+
+const SlideData = [
     {
         thumb: 'http://s.amazeui.org/media/i/demos/bing-1.jpg',
         img: 'http://s.amazeui.org/media/i/demos/bing-1.jpg'
@@ -26,10 +52,32 @@ export default class Home extends React.Component{
         super(props);
     }
 
+    renderNewsCard() {
+        const footer = (
+            <Card.Child role="footer">
+                <a href="javascript: void(0)">Like</a>
+                <a href="javascript: void(0)">Comment</a>
+                <a href="javascript: void(0)">ReadMore</a>
+            </Card.Child>
+        );
+
+
+        return CardData.map((item,i) =>{
+            return(
+                <Card
+                    key={i}
+                    title={item.title}
+                    footer={footer}>
+                    <p>item.content</p>
+                </Card>
+            );
+        });
+    }
+
     renderSlider(){
         return(
             <Slider>
-                {data.map((item,i) => {
+                {SlideData.map((item,i) => {
                     return(
                         <Slider.Item
                             key={i}
@@ -46,10 +94,12 @@ export default class Home extends React.Component{
     render(){
         return(
                  <Container
-                    transition = 'rfl'>
-                    <Group>
+                     className="Home"
+                     scrollable={true}>
+                     <Group className="Slide">
                         {this.renderSlider()}
-                    </Group>
+                     </Group>
+                     {this.renderNewsCard()}
                 </Container>
         );
     }
