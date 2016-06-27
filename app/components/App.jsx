07a31e6afc95d870b009bd7787f3984e85a13da2
chendaole <chendaole@4399inc.com>
2016-06-27 13:07:19 +0800

    add：Container and LefTab

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import { Container ,TabBar, NavBar, OffCanvas , OffCanvasTrigger} from 'amazeui-touch';
 
+import LeftTab from './LefTab.jsx';
 import Home from './Home.jsx';
+import Gear from './Gear.jsx';
 
 import './App.css';
 
@@ -26,7 +28,7 @@ export default class App extends React.Component{
 
     render(){
         const { tab } = this.state;
-        const content= tab === 'home' ? <Home/> : undefined;
+        const content= tab === 'home' ? <Home/> : <Gear/>;
 
         const NavBarWithCanvas = {
             title : NavBarTitle[tab],
@@ -34,43 +36,49 @@ export default class App extends React.Component{
                 icon : 'bars',
                 component : OffCanvasTrigger,
                 isClone : true,
-                offCanvas : <OffCanvas><div><h1>测试</h1></div></OffCanvas>
+                offCanvas : <OffCanvas><LeftTab/></OffCanvas>
             }]
 
         };
 
         return(
             <Container className="ContainerStyle">
-                <NavBar
-                    className="NavBarStyle"
-                    {...NavBarWithCanvas}
-                    amStyle="primary"/>
-                <div className="Content">
-                    {content}
-                </div>
-                <TabBar
-                    amStyle="primary"
-                    onAction={this.handleClickTabBar.bind(this)}>
-                    <TabBar.Item
-                        className="TabBarStyle"
-                        eventKey = "home"
-                        active = {this.state.tab === 'home'}
-                        icon = 'home'
-                        title = '首页'
-                    />
-                    <TabBar.Item
-                        eventKey = 'gear'
-                        active = {this.state.tab === 'gear'}
-                        icon = 'gear'
-                        title = '设置'
-                    />
-                    <TabBar.Item
-                        eventKey = 'info'
-                        active = {this.state.tab === 'info'}
-                        icon="info"
-                        title="信息"
-                        badge={7}/>
-                </TabBar>
+                <Container fill style={{ background : 'white' }}>
+                    <Container className = 'header'>
+                        <NavBar
+                            className="NavBarStyle"
+                            {...NavBarWithCanvas}
+                            amStyle="primary"/>
+                    </Container>
+                    <Container className="body" fill>
+                        {content}
+                    </Container>
+                    <Container className="footer">
+                        <TabBar
+                            amStyle="primary"
+                            onAction={this.handleClickTabBar.bind(this)}>
+                            <TabBar.Item
+                                className="TabBarStyle"
+                                eventKey = "home"
+                                active = {this.state.tab === 'home'}
+                                icon = 'home'
+                                title = '首页'
+                            />
+                            <TabBar.Item
+                                eventKey = 'gear'
+                                active = {this.state.tab === 'gear'}
+                                icon = 'gear'
+                                title = '设置'
+                            />
+                            <TabBar.Item
+                                eventKey = 'info'
+                                active = {this.state.tab === 'info'}
+                                icon="info"
+                                title="信息"
+                                badge={7}/>
+                        </TabBar>
+                    </Container>
+                </Container>
             </Container>
         );
     }
---
+++ app/components/LefTab.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Container } from 'amazeui-touch';
+
+export  default class  LefTab extends React.Component{
+    constructor(props){
+        super(props)
+    }
+
+    render(){
+        return(
+            <Container>
+                <h1>侧边菜单</h1>
+                <h2>侧边菜单</h2>
+                <h2>侧边菜单</h2>
+                <h2>侧边菜单</h2>
+                <h2>侧边菜单</h2>
+                <h2>侧边菜单</h2>
+            </Container>
+        );
+    }
+}
